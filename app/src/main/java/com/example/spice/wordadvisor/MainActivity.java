d331f1ae1d917ffeb7d9498be057cbe53626c3b7
Diogo Monteiro <jdsasm@hotmail.com>
2016-11-12 20:18:55 +0000

    Can add words Can add sequences of words Added main menu

--- app/src/main/java/com/example/spice/wordadvisor/MainActivity.java
+++ app/src/main/java/com/example/spice/wordadvisor/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.spice.wordadvisor;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.io.File;
 import java.util.Objects;
@@ -12,38 +14,24 @@ import static junit.framework.Assert.assertNotNull;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int CURRENT_DATABASE_VERSION = 42;
-    private SQLiteDatabase mDatabase;
-    private File mDatabaseFile;
+    private static MainActivity ins;
+    MainActivity getInstance(){
+        return ins;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ins = this;
         Log.d("DEBUG","Starting debug");
-        DataBaseHelper db = new DataBaseHelper(this);
-        String prevWord = "";
-        String word1 = "The";
-        String word2 = "Rain";
-        String word3 = "Rain";
-        Log.d("TEST","Adding Word1 to Singles");
-        db.addWordToSingles(word1);
-        if(prevWord.equals("")==true){
-            prevWord = word1;
-        }
-        Log.d("TEST","Adding Word2 to Singles");
-        db.addWordToSingles(word2);
-        Log.d("TEST","Adding Word2 to Seq");
-        db.addWordToSequence(prevWord,word2);
-        Log.d("TEST","Adding Word3 to Singles");
-        db.addWordToSingles(word3);
-        Log.d("TEST","Adding Word3 to Seq");
-        db.addWordToSequence(prevWord,word3);
-
-
-
-
 
     }
+
+    public void onClickAddWord(View view){
+        Intent intent = new Intent(getInstance(),AddWord.class);
+        startActivity(intent);
+    }
+
+
 }
